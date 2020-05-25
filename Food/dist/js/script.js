aaf6b8a3812558be1cdcbe62a06af5e5841d36c8
Sabina <sabina.kutsenko@gmail.com>
2020-05-25 20:14:19 +0300

    -

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -44,12 +44,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
         //Timer
 
-        const deadline = '2020-05-27';
+        const deadline = '2020-12-27';
 
         const getTimeRemaining = (endtime) => {
             const t = Date.parse(endtime) - Date.parse(new Date()),
                 days = Math.floor(t / (1000 * 60 * 60 * 24)),
-                hours = Math.floor((t / 1000 * 60 * 60) % 24),
+                hours = Math.floor(t / (1000 * 60 * 60) % 24),
                 minutes = Math.floor((t / 1000 / 60) % 60),
                 seconds = Math.floor((t / 1000) % 60);
 
