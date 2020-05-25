48e6be77d27094407da0103bb7e2e9c5695426f6
Sabina <sabina.kutsenko@gmail.com>
2020-05-25 17:53:45 +0300

    add dir food(L37)

---
+++ Food/src/js/main.js
@@ -0,0 +1,18 @@
+'use script';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('tabheader__items');
+
+        const hideTabContent = () => {
+            tabsContent.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+            });
+        };
+});
